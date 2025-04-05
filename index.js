930493d7cd27c0a6f120bbb04f35f1f3a9fe144f
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-04-05 17:05:28 +0200

    Start writing script, add object

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-let
+const calculator = {
+
+    memory: {
+        num1: null,
+        operator: null,
+        num2: null,
+    },
+
+    display: 0,
+
+    calculate: function(operator){
+        switch(operator){
+            case "+":
+                return this.memory.num1 + this.memory.num2;
+                break
+            case "-":
+                return this.memory.num1 - this.memory.num2;
+                break
+            case "*":
+                return this.memory.num1 * this.memory.num2;
+                break
+            case ":":
+                return this.memory.num1 / this.memory.num2;
+                break;            
+        }
+    }
+
+}
+
